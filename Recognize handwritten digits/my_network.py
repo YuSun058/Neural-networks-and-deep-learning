7b485ea736a73b634a228da2f07566bbe016a8ed
Eric Yu Sun <ericyusun@hotmail.com>
2021-02-24 20:09:52 +0800

    Modify backpropogation

--- Recognize handwritten digits/my_network.py
+++ Recognize handwritten digits/my_network.py
@@ -10,7 +10,7 @@ import random
 
 # Third-party libraries
 import numpy as np
-from pytictoc import TicToc
+from pytictoc import TicToc  # show the running time
 
 # Construct the above neural network as follows
 
@@ -31,7 +31,7 @@ class Network(object):
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, lr, test_data=None):
-        """Train the neural network by mini-batch stochastic gradient descent method.  The "training_data" is a list of tuples "(x,y)" representing the training inputs and desired outputs.  "epochs" specifies how many times we're going to go through the whole training_data in total.  "mini_batch_size" is the number of data used in each updation in SGD.  "lr" is learning rate.  If "test_data" is provided, then the network will be evaluated against the test data after each epoch, and partial progress will be printed out.  This is useful for tracking progress, but it slows things down substantially."""
+        """Train the neural network by mini-batch stochastic gradient descent method.  The 'training_data' is a list of tuples '(x,y)' representing the training inputs and desired outputs.  'epochs' specifies how many times we're going to go through the whole training_data in total.  'mini_batch_size' is the number of data used in each updation in SGD.  'lr' is learning rate.  If 'test_data' is provided, then the network will be evaluated against the test data after each epoch, and partial progress will be printed out.  This is useful for tracking progress, but it slows things down substantially."""
         training_data = list(training_data)
         n = len(training_data)
 
@@ -58,38 +58,36 @@ class Network(object):
     def update_mini_batch(self, mini_batch, lr):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
-        The "mini_batch" is a list of tuples "(x, y)", and "lr"
-        is the learning rate."""
+        The 'mini_batch' is a list of tuples '(x, y)', and 'lr' is the learning rate."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        for x, y in mini_batch:
+        for x, y in mini_batch:  # send one training sample every time
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            # after the for loop, 'nabla_b' and 'nabla_w' contain the parameters after sum_partial derivatives over the training samples in one mini-batch
         self.weights = [w-(lr/len(mini_batch))*nw
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(lr/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
-        """Return a tuple "(nabla_b, nabla_w)" representing the
-        gradient for the cost function C_x.  "nabla_b" and
-        "nabla_w" are layer-by-layer lists of numpy arrays, similar
-        to "self.biases" and "self.weights"."""
+        """Return a tuple '(nabla_b, nabla_w)' representing the gradient for the cost function C_x.  'nabla_b' and 'nabla_w' are layer-by-layer lists of numpy arrays, similar to 'self.biases' and 'self.weights'."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        # feedforward
+        # Feedforward
         activation = x
-        activations = [x]  # list to store all the activations, layer by layer
-        zs = []  # list to store all the z vectors, layer by layer
+        # list to store all the activations, layer by layer and initialize it with the input x
+        activations = [x]
+        zs = []  # list to store all the z vectors, layer by layer and initialize it with an empty list
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+        # Backward pass
+        delta = self.cost_derivative(
+            activations[-1], y) * sigmoid_prime(zs[-1])  # compute the delta for the output layer, note that '*' is component-wise while 'dot' is matrix multiplication
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
@@ -117,7 +115,8 @@ class Network(object):
 
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
+        \partial a for the output activations.
+        Here, since the cost function is half of MSE, then the partial derivatives should be output activations minus the benchmarks y."""
         return (output_activations-y)
 
 
